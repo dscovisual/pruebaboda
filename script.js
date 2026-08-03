065701bad185fd968e4c9bf2da30c26c26f2c2e1
dscovisual <dscovisual@gmail.com>
2026-08-03 20:43:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     const instruccion = document.getElementById("texto-instruccion");
 
     sobre.addEventListener("click", function() {
-        // Alternamos la clase que activa la animación
+     
         sobre.classList.toggle("sobre-abierto");
         
-        // Escondemos el texto de "Toca el sobre" una vez que lo abren
+        
         if (sobre.classList.contains("sobre-abierto")) {
             instruccion.style.opacity = "0";
         } else {
